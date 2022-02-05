bc33ff795cb51d4cb3c7b31f62c68a449936f639
RamaTanikella <88115546+RamaTanikella@users.noreply.github.com>
2022-02-04 22:41:58 -0500

    final

--- 7.1.4-declarative-counter/src/App.js
+++ 7.1.4-declarative-counter/src/App.js
@@ -6,10 +6,12 @@ function App(){
     return (
     <div>
         <h1>{counter}</h1>
-        <h4>{counter}</h4>
+        <h5>{counter}</h5>
     </div>
         
     )
 }
 
 export default App;
+
+//changed h4 tag to h5 just because//
